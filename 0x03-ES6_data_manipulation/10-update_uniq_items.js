f3de2b11136496a2d6c1870eaec039ce274a3b26
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-03-16 01:48:28 +0000

    task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+/**
+ * Changes the quantity of a unique grocery item to 100.
+ * @param {Map<String, number>} map - A map of the name of a
+ * grocery and its quantity.
+ */
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((val, key) => {
+    if (val === 1) {
+      map.set(key, 100);
+    }
+  });
+}
