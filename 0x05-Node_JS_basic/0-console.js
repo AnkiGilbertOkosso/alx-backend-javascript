b9dc3ba1faa0b1ae7f65205e5fd5c0139b79390e
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-04-30 14:39:00 +0100

    task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} message The message to display.
+ * @documentary This function logs a message to the standard output.
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
